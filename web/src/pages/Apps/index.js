6f3b8f4f8eb30f4eb961a925f2ff7979dc530aa7
EddyPBR <edvaldojunior1310@gmail.com>
2020-07-08 11:24:11 -0300

    Services/Apps: Alterando estrutura, imagens e estilo

--- web/src/pages/Apps/index.js
+++ web/src/pages/Apps/index.js
@@ -4,22 +4,28 @@ import "./styles.css";
 
 import Navbar from "../../components/Navbar";
 
+import smartphone from "../../assets/services-app.png";
+
 const Apps = () => {
   return (
     <>
       <Navbar />
 
       <div id="apps-content">
-        <div className="container">
-          <div className="content">
+        <div className="row">
+          <div className="first-column">
             <h1>Aplicativos Mobile Android &amp; IOS</h1>
+            <img src={smartphone} alt="Desenvolvimento de Apps"/>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi et
               diam tincidunt, rhoncus diam a, ornare arcu. Vivamus porta mattis
               leo, nec rhoncus massa sollicitudin et. Donec vel magna id lorem
               sagittis vehicula id sit amet libero.{" "}
             </p>
-            <Link className="button">Adquira Já</Link>
+            <Link className="button" to="/contact">Adquira Já</Link>
+          </div>
+          <div className="last-column">
+            <img src={smartphone} alt="Desenvolvimento de Apps"/>
           </div>
         </div>
       </div>
