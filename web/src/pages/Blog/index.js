7077cb549983dba8dfc4f33d588505a17b1aba25
EddyPBR <edvaldojunior1310@gmail.com>
2020-07-11 15:56:25 -0300

    Pagina Blog & Postbox Component: adicionando estilos para responsividade

--- web/src/pages/Blog/index.js
+++ web/src/pages/Blog/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./styles.css";
 
 import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 import PostBox from "../../components/PostBox";
 
 import exemplo from "../../assets/about-main-background.jpg";
@@ -13,47 +14,57 @@ const Blog = () => {
 
       <div id="blog">
         <div className="container">
-          <PostBox
-            title="Lorem Ipsum Doloar - Uma abordagem X"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
-            postURL="/blog/post-test"
-            author="Edvaldo Junior"
-            authorURL="https://linkedin.com/in/edvaldojuniordev"
-            imageURL={exemplo}
-            postDate="01/01/2020"
-          />
+          <div className="posts-grid">
+            <PostBox
+              title="Lorem Ipsum Doloar - Uma abordagem X"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
+              postURL="/blog/post-test"
+              author="Edvaldo Junior"
+              authorURL="https://linkedin.com/in/edvaldojuniordev"
+              imageURL={exemplo}
+              postDate="01/01/2020"
+            />
 
-          <PostBox
-            title="Lorem Ipsum Doloar - Uma abordagem X"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
-            postURL="/blog/post-test"
-            author="Edvaldo Junior"
-            authorURL="https://linkedin.com/in/edvaldojuniordev"
-            imageURL={exemplo}
-            postDate="01/01/2020"
-          />
+            <PostBox
+              title="Lorem Ipsum Doloar - Uma abordagem X"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
+              postURL="/blog/post-test"
+              author="Edvaldo Junior"
+              authorURL="https://linkedin.com/in/edvaldojuniordev"
+              imageURL={exemplo}
+              postDate="01/01/2020"
+            />
 
-          <PostBox
-            title="Lorem Ipsum Doloar - Uma abordagem X"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
-            postURL="/blog/post-test"
-            author="Edvaldo Junior"
-            authorURL="https://linkedin.com/in/edvaldojuniordev"
-            imageURL={exemplo}
-            postDate="01/01/2020"
-          />
+            <PostBox
+              title="Lorem Ipsum Doloar - Uma abordagem X"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
+              postURL="/blog/post-test"
+              author="Edvaldo Junior"
+              authorURL="https://linkedin.com/in/edvaldojuniordev"
+              imageURL={exemplo}
+              postDate="01/01/2020"
+            />
 
-          <PostBox
-            title="Lorem Ipsum Doloar - Uma abordagem X"
-            text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
-            postURL="/blog/post-test"
-            author="Edvaldo Junior"
-            authorURL="https://linkedin.com/in/edvaldojuniordev"
-            imageURL={exemplo}
-            postDate="01/01/2020"
-          />
+            <PostBox
+              title="Lorem Ipsum Doloar - Uma abordagem X"
+              text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis, elit at imperdiet cursus, sem leo vulputate ligula, ut euismod sem nisi condimentum augue. Sed non leo lectus."
+              postURL="/blog/post-test"
+              author="Edvaldo Junior"
+              authorURL="https://linkedin.com/in/edvaldojuniordev"
+              imageURL={exemplo}
+              postDate="01/01/2020"
+            />
+          </div>
+
+          <button className="button">Carregar Mais Posts</button>
+
+          <div className="insta-grid">
+            {/* Adicionar os posts do instagram */}
+          </div>
         </div>
       </div>
+
+      <Footer />
     </>
   );
 };
