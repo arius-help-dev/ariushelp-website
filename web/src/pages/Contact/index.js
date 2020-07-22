7071b4719ef9861ecca42ec5586520e8e19cfb1d
EddyPBR <edvaldojunior1310@gmail.com>
2020-07-22 14:58:29 -0300

    Pagina Contato: Adicionando formulario de orcamento e responsividade

--- web/src/pages/Contact/index.js
+++ web/src/pages/Contact/index.js
@@ -2,9 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./styles.css";
 
+import CurrencyInput from "react-currency-input";
+
 import contactBackground from "../../assets/contact-background.png";
 
 const Contact = () => {
+  function showEmailForm() {
+    const emailForm = document.getElementById("emailForm");
+    emailForm.classList.remove("hidden");
+  }
+
+  function showBudgetForm() {
+    const budgetForm = document.getElementById("budgetForm");
+    budgetForm.classList.remove("hidden");
+  }
+
+  function hideForms() {
+    const emailForm = document.getElementById("emailForm");
+    const budgetForm = document.getElementById("budgetForm");
+    emailForm.classList.add("hidden");
+    budgetForm.classList.add("hidden");
+  }
+
   function changeForm(event, nameForm) {
     const divFormButtons = document.querySelector(".buttons-row");
     const buttons = [...divFormButtons.getElementsByTagName("button")];
@@ -12,7 +31,8 @@ const Contact = () => {
     buttons.map((button) => button.classList.remove("active"));
     event.target.classList.add("active");
 
-    nameForm === "email" ? console.log("show email") : console.log("show orçamento");;
+    hideForms();
+    nameForm === "email" ? showEmailForm() : showBudgetForm();
   }
 
   return (
@@ -41,7 +61,91 @@ const Contact = () => {
           </div>
         </div>
         <div className="last-column">
-          <h1 id="test">EMAIL</h1>
+
+          <div className="container" id="emailForm">
+            <h2>Email</h2>
+            <p>Dúvias, parcerias, lives? - entre em contato conosco</p>
+            <form>
+              <div className="field-row">
+                <div className="field">
+                  <label>Nome</label>
+                  <input />
+                </div>
+                <div className="field">
+                  <label>Sobrenome</label>
+                  <input />
+                </div>
+              </div>
+              <div className="field-row">
+                <div className="field">
+                  <label>Seu Email</label>
+                  <input type="email" />
+                </div>
+                <div className="field">
+                  <label>Assunto</label>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="field-row">
+                <div className="field">
+                  <label>Mensagem</label>
+                  <textarea placeholder="Sua mensagem..."></textarea>
+                </div>
+              </div>
+              <button className="button">Enviar</button>
+            </form>
+          </div>
+
+          <div className="container hidden" id="budgetForm">
+            <h2>Orçamento</h2>
+            <p>Vamos fazer o seu orçamento</p>
+            <form>
+              <div className="field-row">
+                <div className="field">
+                  <label>Nome</label>
+                  <input />
+                </div>
+                <div className="field">
+                  <label>Sobrenome</label>
+                  <input />
+                </div>
+              </div>
+              <div className="field-row">
+                <div className="field">
+                  <label>Seu Email</label>
+                  <input type="email" />
+                </div>
+                <div className="field">
+                  <label>Assunto</label>
+                  <input type="text" />
+                </div>
+              </div>
+              <div className="field-row">
+                <div className="field">
+                  <label>Valor em R$</label>
+                  <CurrencyInput decimalSeparator="," thousandSeparator="." />
+                </div>
+                <div className="field">
+                  <label>Serviço</label>
+                  <select id="services" name="services">
+                    <option value="apps">Apps</option>
+                    <option value="website">Websites</option>
+                    <option value="eCommerce">E-commerce</option>
+                    <option value="Instaservice">Instaservice</option>
+                    <option value="Instaservice">Outro</option>
+                  </select>
+                </div>
+              </div>
+              <div className="field-row">
+                <div className="field">
+                  <label>Mensagem</label>
+                  <textarea placeholder="Descreva o seu projeto ..."></textarea>
+                </div>
+              </div>
+              <button className="button">Enviar</button>
+            </form>
+          </div>
+
         </div>
       </div>
     </div>
